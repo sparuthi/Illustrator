ecbcb4ccbe18cbd99b664b52b711c139bfbf3328
sparuthi <99080736+sparuthi@users.noreply.github.com>
2022-02-27 23:58:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let PandaStomach = document.getElementById("stomach")
+  PandaStomach.addEventListener("click", ()=>{
+    document.getElementById("tickle").innerText = "You won't listen, will you? Don't do that, it tickles!!"
+  })
